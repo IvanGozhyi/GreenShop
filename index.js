b254d401955b3b2b23bafc43d21d61ba2a0f1fae
Ivan Gozhyi <givan2981@gmail.com>
2025-02-10 20:47:26 +0200

    .

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Happy developing ✨')
+const button= document.getElementById ('MyButton');
+
+
+//Link
+button.addEventListener('click', function(){
+
+    window.location.href = 'index.html';
+
+
+
+})
